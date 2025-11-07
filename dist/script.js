9cd02da47d1bd9bde1908021483798ca3905973a
madelinegarner583 <madelinegarner583@u.boisestate.edu>
2025-11-07 19:57:46 +0000

    working on the lab

--- dist/script.js
+++ dist/script.js
@@ -1,7 +1,7 @@
 document.addEventListener("DOMContentLoaded", function() {
-    const addButton = document.getElementById("add-button");
-    const textBox = document.getElementById("newTask");
-    const form = document.getElementById("form");
+    let addButton = document.getElementById("add-button");
+    let textBox = document.getElementById("newTask");
+    let form = document.getElementById("form");
 
     form.addEventListener("submit", function(event) {
         event.preventDefault();
@@ -19,35 +19,30 @@ document.addEventListener("DOMContentLoaded", function() {
 });
 
 function addBtnClick() {
-    const textBox = document.getElementById("newTask");
-    const userTask = textBox.value.trim();
+    let textBox = document.getElementById("newTask");
+    let userValue = textBox.value.trim();
 
-    if (userTask !== "") {
-        addTask(userTask);
+    if (userValue !== "") {
+        addTask(userValue);
+        textBox.value = "";
+        textBox.focus();
     }
-
-    textBox.value = "";
-    textBox.focus();
 }
 
 function addTask(newTask) {
-    const task = document.createElement("li");
-    const taskList = document.querySelector("ol");
-    const textBox = document.getElementById("newTask");
+    let taskToAdd = document.createElement("li");
+    taskToAdd.innerHTML = `<span class="task-text">${newTask}</span><button class="done-btn">&#10006;</button>`;
+    let taskList = document.querySelector("ol");
+    taskList.appendChild(taskToAdd);
 
-    task.innerHTML = `<span class="task-text">${newTask}</span><button class="done-btn">&#10006;</button>`;
-    taskList.appendChild(task);
-
-    const doneButtons = document.querySelectorAll(".done-btn");
-    const lastDoneButton = doneButtons[doneButtons.length - 1];
-
+    let doneButtons = document.querySelectorAll(".done-btn");
+    let lastDoneButton = doneButtons[doneButtons.length - 1];
     lastDoneButton.addEventListener("click", removeTask);
-    textBox.focus();
 }
 
 function removeTask(event) {
-    const taskToRemove = event.target.parentNode;
-    const list = taskToRemove.parentNode;
+    let taskToRemove = event.target.parentNode;
+    let list = taskToRemove.parentNode;
 
     list.removeChild(taskToRemove);
 }
